794b44e6e49a5361c9da08edd44fcdb631e482c5
orficode21 <120353082+orficode21@users.noreply.github.com>
2025-08-11 14:02:42 +0700

    integrasi database
    - baru integrasi database nya aja (belum diisi)
    - udah bisa bedain role user sama admin (superadmin belum)

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,8 +1,11 @@
 // src/auth.ts
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -13,6 +16,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: "jwt",
   },
   pages: {
-    signIn: "/auth/signin", // halaman login custom lu
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role ?? "user"; // default ke user kalau kosong
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub!;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
   },
 });
